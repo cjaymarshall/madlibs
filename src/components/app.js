d09888c9e4df23472fedb02e89bc0053439521e1
cjaymarshall <cjaymarshall1@hotmail.com>
2019-03-31 09:13:34 -0600

    created header

--- src/components/app.js
+++ src/components/app.js
@@ -3,8 +3,15 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <h1>Botegga Madlibs</h1>
+      <div>
+        <div className="backgroundSkew">
+          <div className="madlib-heading">
+            <h1>Botegga Madlibs</h1>
+            <div className="madlib-subheading">
+              Fill out the fields below and click the generate button<br/>to see the Mad Lib story
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
